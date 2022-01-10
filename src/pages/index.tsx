595a0b9f5ca1247eaba057b4e7f89f9130e0fdbe
dahyun Ham <dahuyn1@gmail.com>
2022-01-10 17:07:03 +0900

    next-redux-wrpper 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,37 @@
 import type { NextPage } from 'next'
 
-import { Counter } from '../features/counter/counter'
+import { wrapper } from "@/app/store";
+import { useAppDispatch, useAppSelector, useInterval } from "@/app/hook";
+
+import { Counter } from "@/features/counter/counter";
+import Clock from "@/features/clock/clock";
+
+import { selectClock, serverRenderClock, startClock } from "@/features/clock/clockSlice";
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const dispatch = useAppDispatch();
+	const { lastUpdate, light } = useAppSelector(selectClock);
+
+  useInterval(() => {
+		dispatch(startClock());
+	}, 1000);
+
   return (
     <div className={styles.container}>
       <Counter />
+      <Clock lastUpdate={lastUpdate} light={light} />
     </div>
   )
 }
+
+export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+	await store.dispatch(serverRenderClock(true));
+
+	return {
+		props: {},
+	};
+});
 
 export default Home
